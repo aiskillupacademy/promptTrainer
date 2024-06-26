54f8d38b9545c9ea4f7647a8dd3de76ea84c0014
aiskillupacademy <160478752+aiskillupacademy@users.noreply.github.com>
2024-06-26 17:15:48 +0000

    Initial commit

--- app.py
+++ app.py
@@ -11,37 +11,39 @@ dspy.configure(lm=gpt3_turbo)
 
 st.title("Prompt Trainer")
 
-def validate_hops(example, pred, trace=None):
-    hops = [example.question] + [outputs.query for *_, outputs in trace if 'query' in outputs]
-
-    if max([len(h) for h in hops]) > 100: return False
-    if any(dspy.evaluate.answer_exact_match_str(hops[idx], hops[:idx], frac=0.8) for idx in range(2, len(hops))): return False
-
-    return True
+def validate_answer(example, pred, trace=None):
+    return example.answer.lower() == pred.answer.lower()
 
 class CoT(dspy.Module):
     def __init__(self):
         super().__init__()
-        self.prog = dspy.ChainOfThought("question -> answer")
+        self.prog = dspy.ChainOfThought("question, voice -> answer")
     
-    def forward(self, question):
-        return self.prog(question=question)
+    def forward(self, question, voice):
+        return self.prog(question=question, voice=voice)
 
 uploaded_file = st.file_uploader("Choose a CSV file")
 input = st.text_input("Ask the question")
+voice = st.selectbox("Selece Voice", ["PAS","FAB","AIDA","QUEST","BAB"])
+
 if st.button("Enter"):
     if uploaded_file:
         df = pd.read_csv(uploaded_file)
+        df.dropna(inplace=True)
+        df.reset_index(drop=True,inplace=True)
     else:
         df = pd.read_csv("C:/Users/Subham/llmteam/rava-llm-team-workflows/Fine tuning dataset - Sheet1.csv")
     qa_pair = []
     for i in range(df.shape[0]):
-        qa_pair.append(dspy.Example(question=df["Input"][i], answer=df["Output"][i]).with_inputs("question"))
+        qa_pair.append(dspy.Example(question=df["Input"][i], voice=df["Framework"][i], answer=df["Output"][i]).with_inputs("question", "voice"))
     with st.spinner():
-        teleprompter = BootstrapFewShot(metric=validate_hops)
+        teleprompter = BootstrapFewShot(metric=validate_answer)
         optimized_program = teleprompter.compile(CoT(), trainset=qa_pair)
-        pred = optimized_program(input)
+        pred = optimized_program(input, voice)
+        original_string = pred.answer
+        updated_string = original_string.replace("\\n", "\n")
         st.header("Predicted answer:")
-        st.write(pred.answer)
-        st.info(f"Answer without training: {gpt3_turbo(input)}")
+        st.write(updated_string)
+        st.header("Answer without training:")
+        st.write(gpt3_turbo(input))
 
